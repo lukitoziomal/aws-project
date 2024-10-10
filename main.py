a928c49d34213861d9a7ced297896fbf9ab772e2
Lukasz <lukasz.luxxxx@gmail.com>
2024-10-10 12:39:35 +0200

    added delete functions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class CLI(cmd.Cmd):
     prompt = ">> "
 
     def do_list(self, arg):
-        read(s3_client, bucket_name, folder_name)
+        read(s3_client, bucket_name, folder_name, arg)
 
     def do_upload(self, arg):
         try:
@@ -19,29 +19,68 @@ class CLI(cmd.Cmd):
             logging.error(e)
 
     def do_find(self, arg):
-        print(arg)
         find(s3_client, bucket_name, "y-wing/", arg)
-        print(arg)
+
+    def do_delete(self, arg):
+        delete(s3_client, bucket_name, "y-wing/" + arg)
+
+    def do_purge(self, arg):
+        delete_all(s3_client, bucket_name, "y-wing/", arg)
+
+    def do_test(self, arg):
+        print(arg.split(" "))
+        for a in arg:
+            print(a)
 
 
-def read(client, b_name, f_name):
+def read(client, b_name, f_name, *args):
     response = client.list_objects_v2(Bucket=b_name, Prefix=f_name)
-    for obj in response["Contents"]:
-        print(obj)
+    if "-a" in args[0] or "-all" in args[0]:
+        for obj in response["Contents"]:
+            print(obj)
+    else:
+        for obj in response["Contents"]:
+            print(obj["Key"])
 
 
 def add(client, b_name, file):
     client.upload_file(file, b_name, "y-wing/" + file)
 
 
+def delete(client, b_name, file):
+    response = client.delete_object(Bucket=b_name, Key=file)
+    print(response)
+
+
+def delete_all(client, b_name, folder, r=""):
+    if r == "":
+        print("Please specify file prefix.")
+    else:
+        response = client.list_objects_v2(Bucket=b_name, Prefix=folder + r)
+        try:
+            totals = len(response["Contents"])
+            for obj in response["Contents"]:
+                name = obj["Key"]
+                client.delete_object(Bucket=b_name, Key=name)
+                print(f"{name}, deleted.")
+            print(f"-- Deleted {totals} files. --")
+        except Exception as e:
+            logging.error(e)
+            print(f"No files starts with {r}.")
+
+
 def find(client, b_name, f_name, r=""):
     response = client.list_objects_v2(Bucket=b_name, Prefix=f_name + r)
-    for obj in response["Contents"]:
-        print(obj)
+    try:
+        for obj in response["Contents"]:
+            print(obj["Key"])
+    except Exception as e:
+        logging.error(e)
+        print(f"No file starts with {r}.")
 
 
 if __name__ == '__main__':
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
     bucket_name = "developer-task"
     folder_name = "y-wing"
 
